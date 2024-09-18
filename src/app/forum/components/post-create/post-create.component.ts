666dd752caa734cda08fcc81089a44b69c214a0e
Gabotw <u202018627@upc.edu.pe>
2024-09-17 20:37:01 -0500

    feat(post-create): add post create component.

---
+++ src/app/forum/components/post-create/post-create.component.ts
@@ -0,0 +1,50 @@
+import {Component, EventEmitter, Input, input, Output, ViewChild} from '@angular/core';
+import {Post} from "../../model/post.entity";
+import {FormsModule, NgForm} from "@angular/forms";
+import {MatFormField} from "@angular/material/form-field";
+
+@Component({
+  selector: 'app-post-create',
+  standalone: true,
+  imports: [
+    MatFormField,
+    FormsModule
+  ],
+  templateUrl: './post-create.component.html',
+  styleUrl: './post-create.component.css'
+})
+export class PostCreateComponent {
+  @Input() post!: Post;
+  @Input() editMode: boolean = false;
+  @Output() protected postAddRequested = new EventEmitter<Post>();
+  @Output() protected postUpdateRequested = new EventEmitter<Post>();
+  @Output() protected cancelRequested = new EventEmitter<void>();
+  @ViewChild('PostForm', {static: false}) protected postForm!: NgForm;
+
+  constructor() {
+    this.post = new Post({});
+  }
+
+  private resetEditState() {
+    this.post = new Post({});
+    this.postForm.reset();
+  }
+
+  private isValid = () => this.postForm.valid;
+
+  protected isEditMode = (): boolean => this.editMode;
+
+  protected onSubmit() {
+    if (this.isValid()) {
+      this.postAddRequested.emit(this.post);
+      this.resetEditState();
+    } else {
+      console.error('Invalid form data');
+    }
+  }
+
+  protected onCancel() {
+    this.cancelRequested.emit();
+    this.resetEditState();
+  }
+}
