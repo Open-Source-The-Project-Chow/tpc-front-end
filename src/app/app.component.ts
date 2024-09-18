1ca96072b78fda3f6c9fba956daca0cd88d6629d
Gabotw <u202018627@upc.edu.pe>
2024-09-17 20:38:23 -0500

    feat(routes): add routes configuration.

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,13 +1,20 @@
 import { Component } from '@angular/core';
-import { RouterOutlet } from '@angular/router';
+import {RouterLink, RouterOutlet} from '@angular/router';
+import {MatToolbar} from "@angular/material/toolbar";
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [RouterOutlet],
+  imports: [RouterOutlet, MatToolbar, RouterLink],
   templateUrl: './app.component.html',
   styleUrl: './app.component.css'
 })
 export class AppComponent {
   title = 'tpc-front-end';
+  protected options = [
+    {path: '/item', title: 'Items'},
+    {path: '/inventory', title: 'Inventory'},
+    {path: '/forum', title: 'Forum'},
+    {path: '/profile', title: 'Profile'},
+    ]
 }
--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,3 +1,12 @@
 import { Routes } from '@angular/router';
+import {PageNotFoundComponent} from "./public/pages/page-not-found/page-not-found.component";
+import {PostManagementComponent} from "./forum/pages/post-management/post-management.component";
 
-export const routes: Routes = [];
+export const routes: Routes = [
+  { path: 'item', component: PageNotFoundComponent},
+  { path: 'inventory', component: PostManagementComponent},
+  { path: 'profile', component: PostManagementComponent},
+  { path: 'forum', component: PostManagementComponent},
+  { path: '',                 redirectTo: 'forum', pathMatch: 'full'},
+  { path: '**',               component: PageNotFoundComponent}
+];
