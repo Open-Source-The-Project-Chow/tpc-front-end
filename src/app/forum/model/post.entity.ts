01b14212f68b08d3ace243ad1307ad0112fdd345
Gabotw <129527889+Gabotw@users.noreply.github.com>
2024-09-20 00:27:20 -0500

    feat(post-entity): update fake api and post entity.

--- src/app/forum/model/post.entity.ts
+++ src/app/forum/model/post.entity.ts
@@ -1,15 +1,19 @@
 export class Post {
   id: number;
   userName: string;
+  image: string;
   title: string;
   date: string;
+  thingImage: string;
   content: string;
 
-  constructor(post: { id?: number, userName?: string, title?: string, date?: string, content?: string}) {
+  constructor(post: { id?: number, userName?: string, image?: string, title?: string, date?: string, thingImage?: string, content?: string}) {
     this.id = post.id || 0;
     this.userName = post.userName || "";
+    this.image = post.image || '';
     this.title = post.title || '';
     this.date = post.date || '';
+    this.thingImage = post.thingImage || '';
     this.content = post.content || '';
   }
 }
