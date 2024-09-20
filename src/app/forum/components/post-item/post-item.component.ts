fbc6a1cc08a8bdf6069fea6303b1dd6e1777dbcd
Gabotw <129527889+Gabotw@users.noreply.github.com>
2024-09-20 00:26:32 -0500

    feat(post-item): add new format to view the cards.

--- src/app/forum/components/post-item/post-item.component.ts
+++ src/app/forum/components/post-item/post-item.component.ts
@@ -1,6 +1,13 @@
 import {Component, Input} from '@angular/core';
 import {Post} from "../../model/post.entity";
-import {MatCard, MatCardContent, MatCardHeader, MatCardTitle, MatCardTitleGroup} from "@angular/material/card";
+import {
+  MatCard, MatCardAvatar,
+  MatCardContent,
+  MatCardHeader, MatCardImage,
+  MatCardTitle,
+  MatCardTitleGroup
+} from "@angular/material/card";
+import {NgStyle} from "@angular/common";
 
 @Component({
   selector: 'app-post-item',
@@ -10,7 +17,10 @@ import {MatCard, MatCardContent, MatCardHeader, MatCardTitle, MatCardTitleGroup}
     MatCardTitleGroup,
     MatCardHeader,
     MatCard,
-    MatCardContent
+    MatCardContent,
+    MatCardAvatar,
+    NgStyle,
+    MatCardImage
   ],
   templateUrl: './post-item.component.html',
   styleUrl: './post-item.component.css'
