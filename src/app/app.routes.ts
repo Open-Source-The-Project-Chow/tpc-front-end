c86e7315c41f1c1faffbd580147509e2c6efa3ce
Diego Flores Avalos <129472574+DiegoFA04@users.noreply.github.com>
2024-09-23 16:33:50 -0500

    feat(inventory): updated routes config.

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -2,10 +2,11 @@ import { Routes } from '@angular/router';
 import {PageNotFoundComponent} from "./public/pages/page-not-found/page-not-found.component";
 import {PostManagementComponent} from "./forum/pages/post-management/post-management.component";
 import {PostViewComponent} from "./forum/components/post-view/post-view.component";
+import {MaterialManagementComponent} from "./inventory/pages/material-management/material-management.component";
 
 export const routes: Routes = [
   { path: 'item', component: PageNotFoundComponent},
-  { path: 'inventory', component: PageNotFoundComponent},
+  { path: 'inventory', component: MaterialManagementComponent},
   { path: 'profile', component: PageNotFoundComponent},
   { path: 'forum', component: PostManagementComponent},
   { path: 'post-view/:id', component: PostViewComponent },
