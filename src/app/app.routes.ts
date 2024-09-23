784a4014498204db81ac70ccf94860247a267151
Marcelo rentería <u202216558@upc.edu.pe>
2024-09-23 16:11:23 -0500

    feat(profile): Added subscriptions component and routes.

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -2,12 +2,15 @@ import { Routes } from '@angular/router';
 import {PageNotFoundComponent} from "./public/pages/page-not-found/page-not-found.component";
 import {PostManagementComponent} from "./forum/pages/post-management/post-management.component";
 import {PostViewComponent} from "./forum/components/post-view/post-view.component";
+import {OwnProfileComponent} from "./profile/components/own-profile/own-profile.component";
+import {SusbcriptionsComponent} from "./profile/components/susbcriptions/susbcriptions.component";
 
 export const routes: Routes = [
   { path: 'item', component: PageNotFoundComponent},
   { path: 'inventory', component: PageNotFoundComponent},
-  { path: 'profile', component: PageNotFoundComponent},
+  { path: 'profile', component: OwnProfileComponent},
   { path: 'forum', component: PostManagementComponent},
+  { path: 'subscriptions', component: SusbcriptionsComponent},
   { path: 'post-view/:id', component: PostViewComponent },
   { path: '',                 redirectTo: 'item', pathMatch: 'full'},
   { path: '**',               component: PageNotFoundComponent}
---
+++ src/app/profile/components/susbcriptions/susbcriptions.component.ts
@@ -0,0 +1,12 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-susbcriptions',
+  standalone: true,
+  imports: [],
+  templateUrl: './susbcriptions.component.html',
+  styleUrl: './susbcriptions.component.css'
+})
+export class SusbcriptionsComponent {
+
+}
