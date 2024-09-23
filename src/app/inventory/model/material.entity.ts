e67912303d11fa1ece9cfca0da082421b9476c73
Diego Flores Avalos <129472574+DiegoFA04@users.noreply.github.com>
2024-09-23 09:26:32 -0500

    feat(inventory): added material entity.

---
+++ src/app/inventory/model/material.entity.ts
@@ -0,0 +1,13 @@
+export class Material {
+  id: number;
+  name: string;
+  quantity: number;
+  stand: string;
+
+  constructor() {
+    this.id = 0;
+    this.name = '';
+    this.quantity = 0;
+    this.stand = '';
+  }
+}
