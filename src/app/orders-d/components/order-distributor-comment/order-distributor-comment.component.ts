93496d7302290a8043f67045fd8cbc0cde2acc22
Salvador Chamorro <u201914367@upc.edu.pe>
2024-09-23 15:10:55 -0500

    feat(orders-d): added order-distributor-comment component

---
+++ src/app/orders-d/components/order-distributor-comment/order-distributor-comment.component.ts
@@ -0,0 +1,12 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-order-distributor-comment',
+  standalone: true,
+  imports: [],
+  templateUrl: './order-distributor-comment.component.html',
+  styleUrl: './order-distributor-comment.component.css'
+})
+export class OrderDistributorCommentComponent {
+
+}
