71ada625ea8ad3eceaae268b683ee76c2bf792f1
SaeBryxn <u202213278@upc.edu.pe>
2024-09-23 16:13:27 -0500

    feat(produc-list)): add class product list component

--- src/app/product-list/product-list.component.ts
+++ src/app/product-list/product-list.component.ts
@@ -2,11 +2,21 @@ import { Component } from '@angular/core';
 
 @Component({
   selector: 'app-product-list',
-  standalone: true,
-  imports: [],
   templateUrl: './product-list.component.html',
-  styleUrl: './product-list.component.css'
+  styleUrls: ['./product-list.component.css'],
+  standalone: true
 })
 export class ProductListComponent {
+  // Simula una lista de productos
+  products = [
+    { id: 1, title: 'Collares cerámica', score: '10/10', name: 'Juan Pérez' },
+    // otros productos...
+  ];
 
+  // Método para manejar el clic en Revisar
+  onReviewClick({product}: { product: any }): void {
+    console.log('Revisar producto:', product);
+    // Aquí puedes agregar lógica para abrir una página de detalles o un modal
+    alert('Revisando producto: ' + product.title);
+  }
 }
