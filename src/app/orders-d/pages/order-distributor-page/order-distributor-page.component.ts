43ffd27bfed4bb34f0857683660d536a4f2a845d
Salvador Chamorro <u201914367@upc.edu.pe>
2024-09-23 15:12:02 -0500

    feat(orders-d): added order-distributor-page component

---
+++ src/app/orders-d/pages/order-distributor-page/order-distributor-page.component.ts
@@ -0,0 +1,42 @@
+import {Component, inject} from '@angular/core';
+import {MatButton, MatIconButton} from "@angular/material/button";
+import {MatIcon} from "@angular/material/icon";
+import {NgForOf, NgIf} from "@angular/common";
+import {MatToolbar} from "@angular/material/toolbar";
+import {MatFormField} from "@angular/material/form-field";
+import {MatCardImage} from "@angular/material/card";
+import {Order} from "../../../orders-d/model/order-d.entity";
+import {MatDialog} from "@angular/material/dialog";
+import {
+  OrderDistributorCommentComponent
+} from "../../components/order-distributor-comment/order-distributor-comment.component";
+
+@Component({
+  selector: 'app-order-distributor-page',
+  standalone: true,
+  imports: [
+    MatButton,
+    MatIcon,
+    MatIconButton,
+    NgForOf,
+    NgIf,
+    MatToolbar,
+    MatFormField,
+    MatCardImage
+  ],
+  templateUrl: './order-distributor-page.component.html',
+  styleUrl: './order-distributor-page.component.css'
+})
+export class OrderDistributorPageComponent {
+  private dialog: MatDialog = inject(MatDialog);
+  openCreatePostDialog(): void {
+    const dialogRef = this.dialog.open(OrderDistributorCommentComponent, {
+      width: '50%',
+      height: 'fit-content',
+      data: { order: new Order({}), editMode: false },
+      disableClose: false,
+      panelClass: 'custom-dialog-container'
+    });
+  }
+
+}
