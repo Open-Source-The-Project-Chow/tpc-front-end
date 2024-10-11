c23b4baf228c0f70cbc45936932dc6f294ec7af8
Salvador Chamorro <u201914367@upc.edu.pe>
2024-10-11 11:16:42 -0500

    feat: updated order-service

--- src/app/orders/services/order.service.ts
+++ src/app/orders/services/order.service.ts
@@ -1,17 +1,23 @@
 import { Injectable } from '@angular/core';
 import {BaseService} from "../../shared/services/base.service";
-import {Post} from "../../forum/model/post.entity";
+import {Order} from "../model/order.entity";
 import {Observable} from "rxjs";
 
 @Injectable({
   providedIn: 'root'
 })
-export class OrderService extends BaseService<Post>{
+export class OrderService extends BaseService<Order>{
+
   constructor() {
     super();
     this.resourceEndpoint = '/order';
   }
-  getOrdertById(id: string): Observable<Post> {
+  getOrderById(id: string): Observable<Order> {
     return this.getById(id);
   }
+  //ajustate de base service
+  updateCommentOrder(order: Order): Observable<Order> {
+    return this.partialUpdate(order.id, order);  // Aquí usamos PUT
+  }
+
 }
