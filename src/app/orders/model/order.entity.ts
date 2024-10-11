ca2d30ad86e1f18961157c2d9a87a35cc88449d2
Salvador Chamorro <u201914367@upc.edu.pe>
2024-10-11 11:17:02 -0500

    feat: updated order-entity

--- src/app/orders/model/order.entity.ts
+++ src/app/orders/model/order.entity.ts
@@ -1,19 +1,36 @@
 export class Order {
   id: number;
-  itemName: string;
+  title: string;
   image: string;
-  thingImage: string;
-  timer: string;
-  artisanName: string;
+  craftsman: string;
+  distributor: string;
+  amount: number;
+  deadline: Date;
+  status: string;
   comment: string;
+  comment_date: Date;
 
-  constructor(order: { id?: number, itemName?: string, image?: string, thingImage?: string, timer?: string, artisanName?: string, comment?: string}) {
-    this.id = order.id || 0;
-    this.itemName = order.itemName || "";
-    this.image = order.image || '';
-    this.thingImage = order.thingImage || '';
-    this.timer = order.timer || '';
-    this.artisanName = order.artisanName || '';
-    this.comment = order.comment || '';
+  constructor(order:{id?: number,
+    title?: string,
+    image?: string,
+    craftsman?: string,
+    distributor?: string,
+    amount?: number,
+    deadline?: Date,
+    status?: string,
+    comment?: string,
+    comment_date?: Date}
+                = {}
+  ) {
+    this.id = order.id|| 0;
+    this.title = order.title|| '';
+    this.image = order.image|| '';
+    this.craftsman = order.craftsman|| '';
+    this.distributor = order.distributor|| '';
+    this.amount = order.amount|| 0;
+    this.deadline = order.deadline|| new Date();
+    this.status = order.status|| '';
+    this.comment = order.comment|| '';
+    this.comment_date = order.comment_date|| new Date();
   }
 }
