a8e740989a2351793041c984891ed0e731d5a207
Gabotw <129527889+Gabotw@users.noreply.github.com>
2024-11-14 15:18:19 -0500

    feat: update post-view component.

--- src/app/forum/components/post-view/post-view.component.ts
+++ src/app/forum/components/post-view/post-view.component.ts
@@ -1,10 +1,12 @@
-import {Component, OnInit} from '@angular/core';
-import {Post} from "../../model/post.entity";
-import {ActivatedRoute, RouterLink} from "@angular/router";
-import {PostService} from "../../services/post.service";
-import {NgOptimizedImage} from "@angular/common";
-import {MatCard, MatCardAvatar, MatCardContent, MatCardImage, MatCardTitle} from "@angular/material/card";
-import {MatButton} from "@angular/material/button";
+import { Component, OnInit } from '@angular/core';
+import { Post } from '../../model/post.entity';
+import { ActivatedRoute, RouterLink } from '@angular/router';
+import { PostService } from '../../services/post.service';
+import {NgForOf, NgOptimizedImage} from '@angular/common';
+import { MatCard, MatCardAvatar, MatCardContent, MatCardImage, MatCardTitle } from '@angular/material/card';
+import { MatButton } from '@angular/material/button';
+import { CommentService } from '../../services/comment.service';
+import { Comment } from '../../model/comment.entity';
 
 @Component({
   selector: 'app-post-view',
@@ -17,22 +19,31 @@ import {MatButton} from "@angular/material/button";
     MatCardAvatar,
     MatCardContent,
     MatCardTitle,
-    RouterLink
+    RouterLink,
+    NgForOf
   ],
   templateUrl: './post-view.component.html',
-  styleUrl: './post-view.component.css'
+  styleUrls: ['./post-view.component.css']
 })
 export class PostViewComponent implements OnInit {
   post!: Post;
+  comments: Comment[] = [];
 
-  constructor(private route: ActivatedRoute, private postService: PostService) {}
+  constructor(private route: ActivatedRoute, private postService: PostService, private commentService: CommentService) {}
 
   ngOnInit(): void {
     const postId = this.route.snapshot.paramMap.get('id');
     if (postId) {
       this.postService.getPostById(postId).subscribe(post => {
         this.post = post;
+        this.loadComments(post.id);
       });
     }
+  }
+
+  loadComments(postId: number): void {
+    this.commentService.getCommentsByPostId(postId).subscribe(comments => {
+      this.comments = comments;
+    });
   }
 }
