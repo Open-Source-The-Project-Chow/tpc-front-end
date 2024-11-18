333ed573bfdcb3f0ce2d5e5e95059d25947bd097
Gabotw <u202018627@upc.edu.pe>
2024-11-17 19:50:31 -0500

    feat: update backend connection.

--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -3,5 +3,5 @@ export const environment = {
   //serverBasePath: 'http://localhost:3000'
   //serverBasePath: 'http://localhost:3000/api/v1'
   //serverBasePath: 'https://my-json-server.typicode.com/Open-Source-The-Project-Chow/fake-api'
-  serverBasePath: 'http://localhost:8080/api/v1'
+  serverBasePath: 'https://tpc-back-end-production.up.railway.app/api/v1'
 };
